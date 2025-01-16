020783900cb7c7f48bb815f15603cc00eb97a3c6
Kaushal Krishna <kaushalkrishna011@gmail.com>
2025-01-16 20:39:20 +0530

    date

--- src/pages/direct/Chat.jsx
+++ src/pages/direct/Chat.jsx
@@ -239,7 +239,7 @@ const Chat = () => {
               const isSingleMessage = !isSameSenderPrev && !isSameSenderNext;
 
               const bgColor = isSelfMessage ? "bg-[#1572db]" : "bg-[#222933]";
-              const baseClasses = "text-left py-2 px-3 break-words rounded-3xl";
+              const baseClasses = "text-left py-2 px-3 break-words rounded-full";
               const borderRadius = isSingleMessage
                 ? "rounded-full"
                 : isSelfMessage
